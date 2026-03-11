452c9b6697cce2c675aac28be8cd9f7104ed514b
patelhet20 <het.patel5700@gmail.com>
2026-03-11 23:17:47 +0530

    all changes

--- db_connect.php
+++ db_connect.php
@@ -1,26 +1,25 @@
+```php
 <?php
-// Start session only if it hasn't been started yet
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-// Replace with your localhost database credentials
-// $host = "localhost";
-// $username = "root";
-// $password = ""; // Empty by default in XAMPP
-// $database = "employee_db";
+$host = "sql100.infinityfree.com";
+$username = "if0_41365881";
+$password = "0A6122001";  // your infinityfree login password
+$database = "if0_41365881_zaverat";
 
-
-// Replace with your Hostinger database credentials
-$host = "127.0.0.1:3306"; // e.g., mysql.hostinger.com
-$username = "u701287743_salary";
-$password = "Het@2210";
-$database = "u701287743_salary";
 $conn = mysqli_connect($host, $username, $password, $database);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+mysqli_set_charset($conn,"utf8mb4");
+
 ?>
-
-
+```
